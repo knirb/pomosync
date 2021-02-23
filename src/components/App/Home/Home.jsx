d1ecfd445898ac1db37aeb33794f2adebfdadf53
Viktor Åsbrink <viktorasbrink@gmail.com>
2021-02-23 23:08:09 +0100

    Create funcitonal home page

--- src/components/App/Home/Home.jsx
+++ src/components/App/Home/Home.jsx
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import http from "services/httpService";
+import { TextField } from "@material-ui/core";
+
 import "styles/Home/Home.scss";
 
 const Home = () => {
   const history = useHistory();
-  const [room, setRoom] = useState("public");
+  const [state, setState] = useState({
+    room: "",
+    username: "",
+  });
   const createNewRoom = async () => {
     const { data: res } = await http.get("/newroom");
     history.push(`/room/${res.room}`);
@@ -14,14 +19,62 @@ const Home = () => {
   useEffect(() => {
     document.body.style = "background-color:#FC5242";
   }, []);
+
+  const handleChange = ({ currentTarget: input }) => {
+    setState((state) => {
+      return { ...state, [input.name]: input.value };
+    });
+  };
   return (
     <div className="Home">
       <h1 style={{ fontSize: "8rem", marginBottom: "2rem" }}>Pomosync</h1>
       <h2>Pomos are best with friends</h2>
-      <Button variant="contained" onClick={createNewRoom}>
+      <div>
+        <div>
+          <TextField
+            className={`home-room-input`}
+            name="username"
+            value={state.username}
+            inputProps={{ style: { color: "white" } }}
+            color="secondary"
+            placeholder="Username"
+            onChange={handleChange}
+            style={{ marginBottom: "1rem" }}
+          />
+        </div>
+        <TextField
+          className={`home-room-input`}
+          name="room"
+          value={state.room}
+          inputProps={{ style: { color: "white" } }}
+          color="secondary"
+          placeholder="Room name"
+          onChange={handleChange}
+          style={{ marginBottom: "1rem" }}
+        />
+      </div>
+      <Button
+        variant="contained"
+        onClick={
+          state.room
+            ? () =>
+                history.push({
+                  pathname: `/room/${state.room}`,
+                  state: {
+                    username: state.username,
+                  },
+                })
+            : createNewRoom
+        }
+      >
         New Room
       </Button>
-      <Button variant="contained" onClick={() => history.push(`/room/${room}`)}>
+      <Button
+        variant="contained"
+        onClick={
+          state.room ? () => history.push(`/room/${state.room}`) : () => {}
+        }
+      >
         Join room
       </Button>
     </div>
